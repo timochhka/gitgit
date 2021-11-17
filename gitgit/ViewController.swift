0af9f609e9628501c30eab919db78a5c783e1bb8
timochhka <94381085+timochhka@users.noreply.github.com>
2021-11-17 22:20:33 +0300

    Initial Commit

---
+++ gitgit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitgit
+//
+//  Created by Тимур Рахимкулов on 17.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
